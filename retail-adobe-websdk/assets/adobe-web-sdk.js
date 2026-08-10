03b75a96a1938cf92409efee55576caa1fefc2a2
veeru-tammisetty <veeru.tammisetty1@gmail.com>
2026-08-10 18:37:08 +1000

    Your commit message here

--- retail-adobe-websdk/assets/adobe-web-sdk.js
+++ retail-adobe-websdk/assets/adobe-web-sdk.js
@@ -15,7 +15,7 @@
 console.log("===== adobe-web-sdk.js STARTED =====");
 
 window.adobeDataLayer = window.adobeDataLayer || [];
-
+ myDataLayer = window.adobeDataLayer;
 console.log("adobeDataLayer initialized:", window.adobeDataLayer);
 
 console.log("Pushing to adobeDataLayer");
@@ -125,7 +125,7 @@ document.getElementById("checkout-form")?.addEventListener("submit", function (e
 
   });
     console.log("adobeDataLayer purchase ingestion:", window.adobeDataLayer);
-    const myDataLayer = window.adobeDataLayer;
+     myDataLayer = window.adobeDataLayer;
 });
 console.log("Data Layer:", myDataLayer);
 console.log("===== add to cart =====");
